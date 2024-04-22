3d16a6478fb83f34de911197ca7500ad02adbe82
IT20622596 <ravbuddhima@gmail.com>
2024-04-22 17:52:59 +0530

    update the route.js file

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const productRouter = require('./routes/product.route');
 const app = express();
 app.use(bodyParser.json());
 
-// Connect to MongoDB
+//Connect to MongoDB
 mongoose.connect(
   'mongodb+srv://sithumgamage180:sithumgamage180@cluster0.tnmn3yf.mongodb.net/productDB'
 );
@@ -14,9 +14,9 @@ const db = mongoose.connection;
 db.on('error', (error) => console.error(error));
 db.once('open', () => console.log('Connected to Database'));
 
-// Routes
+//Routes
 app.use('/products', productRouter);
 
-// Start server
+//Start server
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
