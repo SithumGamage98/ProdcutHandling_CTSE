2b39c5f0efc7f2adaff2a6de847ec645acddc39c
IT20617332 <siridana@gmail.com>
2024-04-22 22:08:14 +0530

    service altered

--- routes/product.route.js
+++ routes/product.route.js
@@ -3,6 +3,7 @@ const express = require('express');
 const router = express.Router();
 const Product = require('../model/product.model');
 
+
 // Get all  products
 router.get('/', async (req, res) => {
   try {
